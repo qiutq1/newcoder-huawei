8784ab80c5866996cdddfe691d32f5235eaf660c
qiutq1 <35127517+qiutq1@users.noreply.github.com>
2022-06-07 20:38:43 +0800

    优化参数 (#1)

--- src/utils/GcdGetter.java
+++ src/utils/GcdGetter.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class GcdGetter {
-    public static int getGcd(int[] nums) {
+    public static int getGcd(int... nums) {
         if (nums.length == 2) {
             int a = nums[0];
             int b = nums[1];
@@ -12,7 +12,7 @@ public class GcdGetter {
                 if (b == 0) return a;
             }
         } else if (nums.length > 2) {
-            int gcd = getGcd(new int[]{nums[0], nums[1]});
+            int gcd = getGcd(nums[0], nums[1]);
             int[] newNumbers = new int[nums.length - 1];
             newNumbers[0] = gcd;
             System.arraycopy(nums, 2, newNumbers, 1, nums.length - 2);
